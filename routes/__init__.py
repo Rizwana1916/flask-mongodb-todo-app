278fc4f6345d2d44d490bac6408d77c52a875542
Rizwana Sheikh <121599047+Rizwana1916@users.noreply.github.com>
2023-08-08 09:13:54 +0530

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,1 @@
-from flask import Blueprint
 
-# Create a blueprint for tasks routes
-tasks_bp = Blueprint('tasks', __name__, url_prefix='/tasks')
-
-# Import routes defined in tasks.py to make them accessible
-from . import tasks
